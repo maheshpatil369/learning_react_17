e112afb90de9894b776ef2663a7c091c26f9a335
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-03-08 14:59:35 +0530

    node modules

---
+++ login-page/src/components/loginfirst.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const loginfirst = () => {
+  return (
+    <div>
+     <h3>Log in</h3>
+
+<input type={type}
+value={value}
+onChange={onchange}
+className="w-full" />
+
+    </div>
+  )
+}
+
+export default loginfirst
+
