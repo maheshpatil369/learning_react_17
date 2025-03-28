ad5c1b258b876956b82cac1b3b094a6cc9b0b249
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-03-28 21:13:45 +0530

    small changes

--- Black Project/src/App.jsx
+++ Black Project/src/App.jsx
@@ -9,7 +9,7 @@ export default function Counter() {
       <div className="flex space-x-4">
         <button 
           className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
-          onClick={() => setCount(count + 1)}
+          onClick={() => setCount(count + 3)}
         >
           Increment
         </button>
