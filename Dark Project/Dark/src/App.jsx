8903c9b02cdfdaa2ea85e0f315d037b68144face
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-03-28 21:14:56 +0530

    Minor changes

--- Dark Project/Dark/src/App.jsx
+++ Dark Project/Dark/src/App.jsx
@@ -30,7 +30,7 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
+            className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-600 transition"
           >
             Login
           </button>
