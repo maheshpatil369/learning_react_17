c8218739a9c34597fa002d34ef39489553aaae0a
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-03-29 23:51:24 +0530

    commit

--- Dark Project/Dark/src/App.jsx
+++ Dark Project/Dark/src/App.jsx
@@ -13,7 +13,7 @@ const Login = () => {
             <input
               type="email"
               id="email"
-              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-5 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your email"
             />
           </div>
